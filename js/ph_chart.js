8a1505eb2cd6aef6e4b2db1981b800961b378214
Katarina <janryanamurao@gmail.com>
2025-03-09 14:36:11 +0800

    initialcommit

---
+++ js/ph_chart.js
@@ -0,0 +1,45 @@
+const ph_ctx = document.getElementById('sample1_chart').getContext('2d');
+// var ph_data
+  new Chart(ph_ctx, {
+    type: 'line',
+    data: {
+      labels: ['data_1', 'data_2', 'data_3', 'data_4', 'data_5', 'data_6'],
+      color: '#fefcfb',
+      datasets: [{
+        label: 'pH',
+        backgroundColor: '#fefcfb',
+        fontColor: '#fefcfb',
+        data: [12, 14, 3, 5, 2, 3],
+        // data: ph_data,
+        borderColor: '#fefcfb',
+        tension: 0.3,
+        borderWidth: 3
+      }]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          display: true,
+          labels: {
+            color: '#fefcfb',
+          },
+        },
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: '#fefcfb'
+          },
+        },
+        y: {
+          ticks:{
+            color: '#fefcfb'
+          },
+          beginAtZero: true,
+          min: 0,
+          max: 15,
+        }
+      }
+  }
+  });
